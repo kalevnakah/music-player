7b9556cb48e29b2dcba7ae3debddffbdd5365f91
kalevnakah <calebschmidt@gmail.com>
2022-06-12 21:28:55 -0500

    Music Player: HTML & CSS

---
+++ script.js
@@ -0,0 +1,4 @@
+const switchBtn = document.getElementById('switch-album');
+const albumTitle = document.getElementById('album-title');
+const albumImageBox = document.getElementById('album-img-box');
+const songList = document.getElementById('switch-album');
